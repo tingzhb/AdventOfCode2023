bc80ab28551244a65134810c849f5f668ab527ba
tingzhb <tingzhb@gmail.com>
2023-12-04 12:14:33 +0100

    S4 Refactor

--- App/S4.cs
+++ App/S4.cs
@@ -2,6 +2,7 @@
 	public static void RunSolution(string fileNumber){
 		var stringArray = File.ReadAllLines($"B:\\Projects\\AdventOfCode2023\\Inputs\\{fileNumber}.txt");
 		var totalPoints = 0;
+		var wonCountList = new List<int>();
 		
 		foreach (var card in stringArray){
 			var cardData = card.Split(':');
@@ -11,37 +12,34 @@
 			var winningNumbers = new List<int>();
 			var scratchNumbers = new List<int>();
 			
-			var wonNumbers = new List<int>();
 			var points = 0;
-
-			foreach (var winningNumber in winningNumberArray){
-				if (int.TryParse(winningNumber, out var number) && number > 0){
-					winningNumbers.Add(number);
-				}
-			}
-
-			foreach (var scratchNumber in scratchNumberArray){
-				if (int.TryParse(scratchNumber, out var number) && number >0){
-					scratchNumbers.Add(number);
-				}
-			}
 			
-			foreach (var winningNumber in winningNumbers){
-				if (scratchNumbers.Contains(winningNumber)){
-					wonNumbers.Add(winningNumber);
-				}
-			}
+			DeleteSpaces(winningNumberArray, winningNumbers);
+			DeleteSpaces(scratchNumberArray, scratchNumbers);
+
+			var wonNumbers = winningNumbers.Where(winningNumber => scratchNumbers.Contains(winningNumber)).ToList();
 			var wonCount = wonNumbers.Count;
-			Console.WriteLine(wonCount);
-			if (wonCount > 0){
-				points = 1;
-				for (var i = 1; i < wonCount; i++){
-					points *= 2;
-				}
-			}
-			totalPoints += points;
+			wonCountList.Add(wonCount);
+			
+			totalPoints = CalculatePoints(wonCount, points, totalPoints);
 		}
 		Console.WriteLine("S4: " + totalPoints);
-		
+	}
+	static int CalculatePoints(int wonCount, int points, int totalPoints){
+		if (wonCount > 0){
+			points = 1;
+			for (var i = 1; i < wonCount; i++){
+				points *= 2;
+			}
+		}
+		totalPoints += points;
+		return totalPoints;
+	}
+	static void DeleteSpaces(string[] winningNumberArray, List<int> winningNumbers){
+		foreach (var winningNumber in winningNumberArray){
+			if (int.TryParse(winningNumber, out var number) && number > 0){
+				winningNumbers.Add(number);
+			}
+		}
 	}
 }
